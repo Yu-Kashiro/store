cec96cbda313aa29b87973c244586723cf197c65
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-09-21 19:07:44 +0900

    製品モデルに説明と在庫数の属性を追加し、フォームと表示を更新。Active StorageとAction Textのサポートを有効化し、アプリケーションのレイアウトを改善。

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
-import "trix"
-import "@rails/actiontext"
+import "trix";
+import "@rails/actiontext";
+
+// Enable Active Storage direct uploads used by Action Text attachments
+import * as ActiveStorage from "@rails/activestorage";
+ActiveStorage.start();
