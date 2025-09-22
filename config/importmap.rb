982ac1e00a0380e3d5d3890968a0c4d53825e8fa
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-09-22 14:19:32 +0900

    ビューの構造を整理し、製品情報をセクションでラップして可読性を向上させました。また、CSSファイルのコメントを削除し、スタイルを整理しました。

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "@rails/activestorage", to: "activestorage.esm.js"
+
+
+pin_all_from "app/javascript/controllers", under: "controllers"
